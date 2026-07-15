22b0f72c3f60c22064cadfe3204be883313aa6ce
ambitionmega1-sudo <ambitionmega1@gmail.com>
2026-07-15 02:22:25 +0100

    Update firebase

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeXN0EUusLBiWkYbYEsQwOVdv9DPTWnEXo",
+  authDomain: "bitionvest.firebaseapp.com",
+  projectId: "bitionvest",
+  storageBucket: "bitionvest.firebasestorage.app",
+  messagingSenderId: "1054566201939",
+  appId: "1:1054566201939:web:480bef86c9e91e5db2a4a6"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
